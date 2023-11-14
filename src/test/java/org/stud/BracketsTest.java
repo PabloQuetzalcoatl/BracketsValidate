471367cacc600a685b86832330e880045d647aac
Pablo <Pablo@pokemail.net>
2023-11-14 12:34:12 +0300

    fix tests for new parameters (open  and close bracket symbol)

--- src/test/java/org/stud/BracketsTest.java
+++ src/test/java/org/stud/BracketsTest.java
@@ -6,18 +6,18 @@ public class BracketsTest {
 
     @org.junit.Test
     public void checkBrackets1() {
-        boolean r = Brackets.checkBrackets("(1+1)=(3-(2+2-2))");
+        boolean r = Brackets.checkBrackets("(1+1)=(3-(2+2-2))",'(', ')');
         assertEquals(true,r);
     }
 
     @org.junit.Test
     public void checkBrackets2() {
-        boolean r = Brackets.checkBrackets("(1+1))=(3-2)");
+        boolean r = Brackets.checkBrackets("(1+1))=(3-2)",'(', ')');
         assertEquals(false,r);
     }
     @org.junit.Test
     public void checkBrackets3() {
-        boolean r = Brackets.checkBrackets("))(((1+1))=(3-2)");
+        boolean r = Brackets.checkBrackets("))(((1+1))=(3-2)",'(', ')');
         assertEquals(false,r);
     }
 }
